e940d435aa57d468e8fb56a1ade149e6017fc954
Cremelado <juan.dadiaz75@gmail.com>
2025-05-03 00:15:00 -0500

    last commit (i guess)

--- src/Components/organism/itemShop/ItemShop.tsx
+++ src/Components/organism/itemShop/ItemShop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./ItemShop.css";
 
 import potion from "../../../assets/potion.png";
--- src/Components/organism/player/Player.tsx
+++ src/Components/organism/player/Player.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import player from "../../../assets/TopPlayers/player Icon.png";
 import "./Player.css";
 import { CircleStackIcon } from "@heroicons/react/24/solid";
@@ -17,16 +16,20 @@ const Player = ({ name, exp, gold, rank, isPoisoned }: PlayerProps) => {
     <div className="card-player cursor-pointer px-1 py-2 md:px-8 md:py-2">
       <div className="flex mx-auto justify-between items-center w-full h-full gap-1 md:gap-4">
         <div className="flex items-center gap-1 md:gap-4">
-        <h1 className="player-name text-3xl" data-text={rank}>
-          {rank}
-        </h1>
-        <img className="w-16 bg-teal-50 rounded-full" src={player} alt="player Icon" />
-        <div className="flex flex-col">
-          <p className="text-base md:text-2xl player-name" data-text={name}>
-            {name}
-          </p>
-          <p className="text-orange-50 text-shadow-lg/20">{exp} EXP</p>
-        </div>
+          <h1 className="player-name text-3xl" data-text={rank}>
+            {rank}
+          </h1>
+          <img
+            className="w-16 bg-teal-50 rounded-full"
+            src={player}
+            alt="player Icon"
+          />
+          <div className="flex flex-col">
+            <p className="text-base md:text-2xl player-name" data-text={name}>
+              {name}
+            </p>
+            <p className="text-orange-50 text-shadow-lg/20">{exp} EXP</p>
+          </div>
         </div>
         <div className="flex md:gap-2">
           <div className="gold-icon bg-orange-950 rounded-full p-1">
@@ -34,10 +37,9 @@ const Player = ({ name, exp, gold, rank, isPoisoned }: PlayerProps) => {
           </div>
           <p className="text-sm md:text-lg gold-ammount">{gold}</p>
           {isPoisoned && (
-          <BeakerIcon className="text-lime-500 w-8 bg-orange-50 shadow-xl/30 rounded-full p-1" />
-        )}
+            <BeakerIcon className="text-lime-500 w-8 bg-orange-50 shadow-xl/30 rounded-full p-1" />
+          )}
         </div>
-        
       </div>
     </div>
   );
